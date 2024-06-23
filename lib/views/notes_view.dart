cca23ab413cace5e152582325957bbcefe8e96e3
G-TECH <haya98166@gmail.com>
2024-06-23 21:20:48 +0300

    App bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -5,6 +5,31 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar:  AppBar(
+        title: const Text('Notes',style: TextStyle(
+            fontSize: 25,
+        ),),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 46,
+              width: 46,
+              decoration:  BoxDecoration(
+                color: Colors.white.withOpacity(0.05),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: const Icon(Icons.search,size: 28,),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [],
+      ),
+
+    );
   }
 }
+
