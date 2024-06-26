81e6383a618a6755702848cbe96c8d81e3786d38
G-TECH <haya98166@gmail.com>
2024-06-26 08:06:52 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel{
+ late final String title;
+ late final String subTitle;
+ late final String date;
+ late final int color;
+ NoteModel({required this.title,required this.subTitle,required this.color,required this.date});
+
+}
