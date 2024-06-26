90c1e00ecd8e72cd929e75abbcd8299242c74781
G-TECH <haya98166@gmail.com>
2024-06-26 09:18:13 +0300

    validate user input

--- lib/widgets/add_note_show_bottom_sheet.dart
+++ lib/widgets/add_note_show_bottom_sheet.dart
@@ -11,20 +11,62 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding:  EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
+      child: SingleChildScrollView(
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey=GlobalKey();
+  AutovalidateMode autovalidateMode=AutovalidateMode.disabled;
+  String? title,subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child:  Column(
         children: [
-          SizedBox(height: 32,),
+          const SizedBox(height: 32,),
           CustomTextField(
+            onSaved: (value){
+              title=value;
+            },
             hint:"Title"
           ),
-          SizedBox(height:16),
+          const SizedBox(height:16),
           CustomTextField(
+            onSaved: (value){
+              subTitle=value;
+            },
               hint:"Content",
               maxLines: 5,
           ),
-          SizedBox(height: 92,),
-          CustomButton(),
+          const SizedBox(height: 92,),
+           CustomButton(
+             onTap: (){
+               if(formKey.currentState!.validate()){
+                 formKey.currentState!.save();
+               }else{
+                 autovalidateMode=AutovalidateMode.always;
+                 setState(() {
 
+                 });
+               }
+             },
+           ),
+      
         ],
       ),
     );
